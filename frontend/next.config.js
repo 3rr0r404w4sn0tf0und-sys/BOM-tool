eb21e186a4d12bc949f236c6ea25e511d525bc12
3rr0r404w4sn0tf0und-sys <3rr0r404w4sn0tf0und@gmail.com>
2026-08-18 16:26:44 -0400

    fixing

--- frontend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // static export -- Cloudflare Pages serves this directly, no Next server needed
-  images: { unoptimized: true },
-};
-
-module.exports = nextConfig;
